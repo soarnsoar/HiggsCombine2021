db890ac3535faa1415a86992b1073e87b74fce14
your name your last name <soarnsoar@gmail.com>
2021-08-25 10:00:06 +0900

    add 3yrs to combine cards

--- Submit_CombineCard.sh
+++ Submit_CombineCard.sh
@@ -1,11 +1,11 @@
 ARR_MASS=(115 120 124 125 126 130 135 140 145 150 155 160 165 170 175 180 190 200 210 230 250 270 300 350 400 450 500 550 600 650 700 750 800 900 1000 1500 2000 2500 3000 4000 5000)
 ARR_BST=(all Boosted Resolved)
-ARR_YEAR=(2016 2017 2018)
+ARR_YEAR=(2016 2017 2018 3yrs)
 
 for MASS in ${ARR_MASS[@]};do
     for BST in ${ARR_BST[@]};do
 	for YEAR in ${ARR_YEAR[@]};do
-	    python CondorSubmit_CombineCard.py -y ${YEAR} -m ${MASS} -b ${BST} #--dnn
+	    python CondorSubmit_CombineCard.py -y ${YEAR} -m ${MASS} -b ${BST} --dnn
 	done
     done
 done
