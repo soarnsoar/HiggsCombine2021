7156c1e29037366e0ad5424fe349aaa9bbb1aa5b
your name your last name <soarnsoar@gmail.com>
2021-10-19 17:43:49 +0900

    update rebinning tool

---
+++ Rebinning/RebinningConfig.py
@@ -0,0 +1,10 @@
+config={
+        "hww_lqq_bst_ggf_2016": [            1,            2,            3,            4        ],
+        "hww_lqq_ggf_2016": [            1,            23,            24        ],
+        "hww_lqq_vbf_2016": [            22,            24        ],
+        "hww_lqq_bst_ggf_2017": [            1,            2,            3,            4        ],
+        "hww_lqq_ggf_2017": [    1        ],
+        "hww_lqq_bst_ggf_2018": [            1,            2,            3,            4        ],
+        "hww_lqq_ggf_2018": [            1,            24        ],
+        "hww_lqq_vbf_2018": [            24        ],
+}
