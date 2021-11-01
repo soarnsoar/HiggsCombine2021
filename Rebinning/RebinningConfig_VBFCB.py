453ae1ac4e9254d615ace52972265977859ebdad
your name your last name <soarnsoar@gmail.com>
2021-11-01 23:54:38 +0900

    add vbfcb

---
+++ Rebinning/RebinningConfig_VBFCB.py
@@ -0,0 +1,13 @@
+config={
+    "hww_lqq_bst_ggf_2016": [            1,            2,            3,            4        ],
+    "hww_lqq_ggf_2016": [            1,                        24        ],
+    "hww_lqq_vbf_2016": [            22,        23,    24        ],
+    "hww_lqq_bst_ggf_2017": [            1,            2,            3,            4        ],
+    "hww_lqq_bst_vbf_2017": [    13        ],
+    "hww_lqq_ggf_2017":[1],
+    "hww_lqq_vbf_2017":[21,23,24],
+    "hww_lqq_bst_ggf_2018": [            1,            2,            3,            4        ],
+    "hww_lqq_ggf_2018": [            1,            24        ],
+    "hww_lqq_untag_2018":[23,24],
+    "hww_lqq_vbf_2018": [         21,23,   24        ],
+}
