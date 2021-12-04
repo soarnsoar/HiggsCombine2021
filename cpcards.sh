01f78035e242c271ce61c0ab6cff381a9974815c
your name your last name <soarnsoar@gmail.com>
2021-12-04 23:54:05 +0900

    submit scripts

---
+++ cpcards.sh
@@ -0,0 +1,3 @@
+cp -r /cms_scratch/jhchoi/Optimization_VBFDNN_211123/2016/Datacards Datacards_2016&
+cp -r /cms_scratch/jhchoi/Optimization_VBFDNN_211123/2017/Datacards Datacards_2017&
+cp -r /cms_scratch/jhchoi/Optimization_VBFDNN_211123/2018/Datacards Datacards_2018&
