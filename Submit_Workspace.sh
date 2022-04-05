05bf95c258fc9c968f6e08cfec3dcf904fc541e4
your name your last name <soarnsoar@gmail.com>
2022-04-05 12:33:01 +0900

    add cuts for step2

--- Submit_Workspace.sh
+++ Submit_Workspace.sh
@@ -4,7 +4,9 @@ ARR_BST=(Boosted Resolved)
 ARR_YEAR=(2016 2017 2018 )
 #ARR_YEAR=(2016 2018 3yrs)
 #ARR_YEAR=(2017)
-ARR_CUT=(0.05 0.1 0.15 0.2 0.25 0.3 0.35 0.4 0.45 0.5 0.55 0.6 0.65 0.7 0.75 0.8 0.85 0.9 0.95 nocut)
+#ARR_CUT=(0.05 0.1 0.15 0.2 0.25 0.3 0.35 0.4 0.45 0.5 0.55 0.6 0.65 0.7 0.75 0.8 0.85 0.9 0.95 nocut)
+ARR_CUT=(0.62 0.64 0.66 0.68 0.7 0.72 0.74 0.76 0.78 0.8 0.82 0.84 0.86 0.88 0.9 0.92 0.94 0.96 0.98 nocut)
+
 #CondorSubmit_MakeWorkSpace.py
 for MASS in ${ARR_MASS[@]};do
     for BST in ${ARR_BST[@]};do
