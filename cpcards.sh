01a7188d55344add7371a776aaae46fb791b9e80
your name your last name <soarnsoar@gmail.com>
2022-04-18 17:44:34 +0900

    cp path

--- cpcards.sh
+++ cpcards.sh
@@ -1,3 +1,13 @@
-cp -r /cms_scratch/jhchoi/VBF_OPT_22/2016/Datacards Datacards_2016&
-cp -r /cms_scratch/jhchoi/VBF_OPT_22/2017/Datacards Datacards_2017&
-cp -r /cms_scratch/jhchoi/VBF_OPT_22/2018/Datacards Datacards_2018&
+#VBF_OPT_22_V10
+ARR_BST=(Boosted Resolved)
+for BST in ${ARR_BST[@]};do
+    cp -r /cms_scratch/jhchoi/VBF_OPT_22_V10/2016/Datacards_${BST} Datacards_${BST}_2016&
+    cp -r /cms_scratch/jhchoi/VBF_OPT_22_V10/2017/Datacards_${BST} Datacards_${BST}_2017&
+    cp -r /cms_scratch/jhchoi/VBF_OPT_22_V10/2018/Datacards_${BST} Datacards_${BST}_2018&
+    
+
+done
+
+    cp -r /cms_scratch/jhchoi/VBF_OPT_22_V10/WJETCR/2016/Datacards_Boosted Datacards_2016_Boosted_SB&
+    cp -r /cms_scratch/jhchoi/VBF_OPT_22_V10/WJETCR2017/Datacards_Boosted Datacards_2017_Boosted_SB&
+    cp -r /cms_scratch/jhchoi/VBF_OPT_22_V10/WJETCR/2018/Datacards_Boosted Datacards_2018_Boosted_SB&
