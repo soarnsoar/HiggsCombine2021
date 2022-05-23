c3696390f764119d11c85110a625d59eee920659
your name your last name <soarnsoar@gmail.com>
2022-05-24 01:59:12 +0900

    a

--- Impact/SubmitImpactJobsCollect.sh
+++ Impact/SubmitImpactJobsCollect.sh
@@ -1,10 +1,16 @@
 #####
 ARR_Year=(2016 2017 2018)
-#ARR_Year=(2017 2018)
 #ARR_Year=(2016)
 Mass=1000
 #SF_Bin2,SF_Bin1,SF_Bin0
 ARR_POI=(SF_Bin0 SF_Bin1 SF_Bin2)
+
+
+#combineTool.py -M Impacts -d htt_tt.root -m 125 -o impacts.json
+
+#plotImpacts.py -i impacts.json -o impacts
+
+
 for Year in ${ARR_Year[@]};do
     Workspace=${PWD}/../Datacards_${Year}/WS_${Year}.root
 
@@ -14,7 +20,9 @@ for Year in ${ARR_Year[@]};do
 	WORKDIR=${Year}__${POI}
 	mkdir -p $WORKDIR
 	cd ${WORKDIR}
-	(combineTool.py -d ${Workspace} -M Impacts -m ${Mass} --doInitialFit  --redefineSignalPOIs ${POI} --setParameter ${POI}=1 --robustFit 1 -n Impact${Year} --robustHesse=1&&combineTool.py -d ${Workspace} -M Impacts -m ${Mass} --doFits --robustFit 1 -n Impact${Year} --redefineSignalPOIs ${POI} && combineTool.py -M Impacts -d ${Workspace} -m ${Mass} -n Impact${Year} -o impacts.json --redefineSignalPOIs ${POI} && plotImpacts.py -i impacts.json -o impacts) &> impact.log&
+	#(combineTool.py -d ${Workspace} -M Impacts -m ${Mass} --doInitialFit  --redefineSignalPOIs ${POI} --robustFit 1 -n Impact${Year}&&combineTool.py -d ${Workspace} -M Impacts -m ${Mass} --doFits --robustFit 1 -n Impact${Year} --redefineSignalPOIs ${POI}) &> impact.log&
+	combineTool.py -M Impacts -d ${Workspace} -m 1000 -n Impact${Year} --redefineSignalPOIs ${POI} -o impacts.json
+	plotImpacts.py -i impacts.json -o impacts
 	cd -
     done
 
